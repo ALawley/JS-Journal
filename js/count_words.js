6bdf2a352aec363efcc42942349d4158a2be11d5
Anna Lawley and Matt Rosanio <alawley1@gmail.com>
2016-03-14 08:30:19 -0700

    create function to count words in input text

---
+++ js/count_words.js
@@ -0,0 +1,10 @@
+exports.countWords = function(userText) {
+  var textSplit = userText.split(" ");
+  for (var i = 0; i < textSplit.length; i++) {
+    if (textSplit[i] === "") {
+      textSplit = textSplit.splice(i, 1);
+      i--;
+    }
+  }
+  return textSplit.length;
+}
