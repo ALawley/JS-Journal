df41a3af288c2c5f52cf742997b4d432b0c67b84
Anna Lawley and Matt Rosanio <alawley1@gmail.com>
2016-03-14 09:11:36 -0700

    add date to journal entries

--- js/browser.js
+++ js/browser.js
@@ -1,10 +1,11 @@
 var countWords = require('./count_words.js').countWords;
+var moment = require('moment');
 
 $(function() {
   $('#journal-entry').submit(function(event) {
     event.preventDefault();
     var title = $('#title').val();
     var body = $('#body').val();
-    $('.show-journal').append("<h3>" + title + "</h3><p>" + body + "</p><p>" + countWords(body) + "</p>");
+    $('.show-journal').append("<h3>" + title + "</h3><p>" + body + "</p><p>" + countWords(body) + "</p><p>Created on: " + moment().format("MMM Do YY") + "</p>");
   });
 });
--- js/cli.js
+++ js/cli.js
@@ -1,5 +1,6 @@
 var countWords = require('./count_words.js').countWords;
 var prompt = require('prompt');
+var moment = require('moment');
 prompt.start();
 
 prompt.get('userText', function(err, results) {
